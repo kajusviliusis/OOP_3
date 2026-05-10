2fe80d7e749ddf6034d87299130d5c7b8df2f9ec
kajusviliusis <kajus.viliusis0@gmail.com>
2026-05-10 22:10:12 +0300

    add vector constructors and operators

--- include/Vector.h
+++ include/Vector.h
@@ -2,6 +2,7 @@
 #define VECTOR_H
 
 #include <cstddef>
+#include <initializer_list>
 
 template <typename T>
 class Vector {
@@ -21,6 +22,96 @@ public:
     using const_pointer = const value_type*;
     using iterator = value_type*;
     using const_iterator = const value_type*;
+
+    //member functions
+    // default konstruktorius
+    Vector() : data_(nullptr), size_(0), capacity_(0) {}
+
+    //konstruktorius su pradiniu size
+    explicit Vector(size_type count)
+        : data_(count > 0 ? new value_type[count]{} : nullptr),
+          size_(count),
+          capacity_(count) {}
+
+    //initializer list konstruktorius
+    Vector(std::initializer_list<value_type> values)
+        : data_(values.size() > 0 ? new value_type[values.size()] : nullptr),
+          size_(values.size()),
+          capacity_(values.size())
+    {
+        size_type i = 0;
+        for (const auto& value : values) {
+            data_[i] = value;
+            i++;
+        }
+    }
+
+    //copy konstruktorius
+    Vector(const Vector& other)
+        : data_(other.size_ > 0 ? new value_type[other.size_] : nullptr),
+          size_(other.size_),
+          capacity_(other.size_)
+    {
+        for (size_type i = 0; i < size_; i++) {
+            data_[i] = other.data_[i];
+        }
+    }
+
+    //copy assignment operatorius
+    Vector& operator=(const Vector& other)
+    {
+        if (this == &other) return *this;
+
+        delete[] data_;
+
+        data_ = other.capacity_ > 0 ? new value_type[other.capacity_] : nullptr;
+        size_ = other.size_;
+        capacity_ = other.capacity_;
+
+        for (size_type i = 0; i < size_; i++) {
+            data_[i] = other.data_[i];
+        }
+
+        return *this;
+    }
+
+    //move konstruktorius
+    Vector(Vector&& other) noexcept
+        : data_(other.data_),
+          size_(other.size_),
+          capacity_(other.capacity_)
+    {
+        other.data_ = nullptr;
+        other.size_ = 0;
+        other.capacity_ = 0;
+    }
+
+    //move assignment operatorius
+    Vector& operator=(Vector&& other) noexcept
+    {
+        if (this == &other) return *this;
+
+        delete[] data_;
+        data_ = other.data_;
+        size_ = other.size_;
+        capacity_ = other.capacity_;
+
+        other.data_ = nullptr;
+        other.size_ = 0;
+        other.capacity_ = 0;
+
+        return *this;
+    }
+
+    ~Vector()
+    {
+        delete[] data_;
+    }
+
+
+
+
+
 };
 
 #endif
